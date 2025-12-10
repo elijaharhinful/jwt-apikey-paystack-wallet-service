5b683c378544da95f26fb53ed6ef9e1d0d5e0f59
Elijah Arhinful <elijaharhinful8@gmail.com>
2025-12-10 07:45:35 +0000

    chore: remove create user endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,20 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserDocs, CreateUserDocs, FindUserDocs } from './docs/users.swagger';
+import { UserDocs, FindUserDocs } from './docs/users.swagger';
 
 @Controller('users')
 @UserDocs()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  // Minimal exposure for debugging or admin
-
-  @Post()
-  @CreateUserDocs()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get(':id')
   @FindUserDocs()
